04707039977cabbdd7d4672f8e3c080fee27f3f3
Periyot <88425310+Periyot@users.noreply.github.com>
2021-09-10 16:26:53 +0300

    beginning

---
+++ QR code generator/QR_code_generatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_code_generatorApp.swift
+//  QR code generator
+//
+//  Created by l on 10.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct QR_code_generatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
